c49088e7b4e4ddd248eed95de1c71eac47be4565
Parth282003 <144408916+Parth282003@users.noreply.github.com>
2025-04-02 00:52:32 +0530

    increased no of recommendatoin

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def fetch_poster(suggestion):
 def recommend_book(book_name):
   book_list=[]
   book_id=np.where(pivot.index==book_name)[0][0]
-  distance,suggestion=model.kneighbors(pivot.iloc[book_id,:].values.reshape(1,-1),n_neighbors=6)
+  distance,suggestion=model.kneighbors(pivot.iloc[book_id,:].values.reshape(1,-1),n_neighbors=11)
   
   poster_url= fetch_poster(suggestion)
   for i in range(len(suggestion)):
@@ -42,7 +42,7 @@ selected_book=st.selectbox("Type or select a book from the dropdown",books)
 
 if st.button('Show Recommendation'):
     recommended_books,poster_url= recommend_book(selected_book)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10  = st.columns(10)
     with col1:
         st.text(recommended_books[1])
         st.image(poster_url[1])
@@ -59,3 +59,18 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_books[5])
         st.image(poster_url[5])
+    with col6:
+        st.text(recommended_books[6])
+        st.image(poster_url[6])
+    with col7:
+        st.text(recommended_books[7])
+        st.image(poster_url[7])
+    with col8:
+        st.text(recommended_books[8])
+        st.image(poster_url[8])
+    with col9:
+        st.text(recommended_books[9])
+        st.image(poster_url[9])
+    with col10:
+        st.text(recommended_books[10])
+        st.image(poster_url[10])
